63252a292d02c3af49f5dfd80f85c8f7c98eaa41
kiminjoong <ijboym216@gmail.com>
2019-02-24 11:34:55 +0900

    Presenter Structure
    2019-02-24 Presenter Structure

--- src/Routes/Detail/DetailPresenter.js
+++ src/Routes/Detail/DetailPresenter.js
@@ -1,1 +1,13 @@
-export default () => "Deatail";
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+
+const DetailPResenter = ({ result, loading, error }) => null;
+
+DetailPResenter.propTypes = {
+    result: PropTypes.object, 
+    loading: PropTypes.bool.isRequired, 
+    error: PropTypes.string
+};
+
+export default DetailPResenter;
--- src/Routes/TV/TVPresenter.js
+++ src/Routes/TV/TVPresenter.js
@@ -1,1 +1,15 @@
-export default () => "TV";
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+
+const TVPResenter = ({ topRated, popular, airingToday, loading, error }) => null;
+
+TVPResenter.propTypes = {
+    topRated: PropTypes.array, 
+    popular: PropTypes.array, 
+    airingToday: PropTypes.array, 
+    loading: PropTypes.bool.isRequired, 
+    error: PropTypes.string
+};
+
+export default TVPResenter;
