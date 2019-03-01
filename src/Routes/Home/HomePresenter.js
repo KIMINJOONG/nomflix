fabedf99d0acbc343f63043f75b18bd33e46bb06
KIMINJOONG <ijboym216@gmail.com>
2019-03-01 14:06:34 +0900

    React Helmet

--- src/Routes/Home/HomePresenter.js
+++ src/Routes/Home/HomePresenter.js
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
+import Helmet from "react-helmet";
 import Section from "Components/Section";
 import Loader from "../../Components/Loader";
 import Message from "../../Components/Message";
@@ -10,8 +11,15 @@ const Container = styled.div`
     padding: 20px;
 `;
 
-const HomePResenter = ({ nowPlaying, popular, upcoming, loading, error }) => loading ? <Loader/> :
-    <Container>
+const HomePResenter = ({ nowPlaying, popular, upcoming, loading, error }) => 
+<>
+    <Helmet>
+        <title>Movies | Nomflix</title>
+    </Helmet>
+    {loading ? (
+        <Loader/>
+    ):
+    (<Container>
         {nowPlaying && nowPlaying.length > 0 && ( 
             <Section title="Now Playing">
                 {nowPlaying.map(movie => (
@@ -58,8 +66,8 @@ const HomePResenter = ({ nowPlaying, popular, upcoming, loading, error }) => loa
             </Section>
         )}
         {error && <Message color="#e74c3c" text={error} />}
-    </Container>;
-
+    </Container>)}
+</>
 HomePResenter.propTypes = {
     nowPlaying: PropTypes.array, 
     popular: PropTypes.array, 
