23a0c84a69935bcd4b94dfaa3d8a38f74068076b
KIMINJOONG <ijboym216@gmail.com>
2019-03-02 11:15:17 +0900

    change path

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -1,8 +1,8 @@
 import React from "react";
-import {BrowserRouter as Router, Route, Redirect, Switch } from "react-router-dom";
+import {HashRouter as Router, Route, Redirect, Switch } from "react-router-dom";
 import Home from "Routes/Home";
 import TV from "Routes/TV";
-import Header from "Components/Header";
+import Header from "../Components/Header";
 import Search from "Routes/Search";
 import Detail from "Routes/Detail";
 
@@ -11,9 +11,9 @@ export default () => (
         <>
             <Header />
             <Switch>
-                <Route path="/" exact component={Home} />
-                <Route path="/tv" exact component={TV} />
-                <Route path="/search" exact component={Search} />
+                <Route path="/" component={Home} />
+                <Route path="/tv" component={TV} />
+                <Route path="/search" component={Search} />
                 <Route path="/movie/:id" component={Detail} />
                 <Route path="/show/:id" component={Detail} />
                 <Redirect from="*" to="/" />
