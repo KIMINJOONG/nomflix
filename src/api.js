60be5b93b847e75e425295bd9ea68ffc0f30af42
KIMINJOONG <ijboym216@gmail.com>
2019-03-17 00:48:33 +0900

    check the browser language and change language value in axios.create params

--- src/api.js
+++ src/api.js
@@ -1,10 +1,11 @@
 import axios from "axios";
 
+
 const api = axios.create({
     baseURL: "https://api.themoviedb.org/3/"
     , params: {
         api_key: "39a55128d46ad2cb9a99830763fbec2d",
-        language: "ko-KR"
+        language: navigator.language || navigator.userLanguage
     }
 });
 
